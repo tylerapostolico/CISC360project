f383eaefe5180dd756c5cf0521eb47749f2d62aa
ckharr <ckharr@udel.edu>
2016-05-12 14:33:18 -0400

    Main.cpp and RandGraph.cpp with comments by Connor

--- Main.cpp
+++ Main.cpp
@@ -6,36 +6,39 @@
 #include <time.h>
 using namespace std;
 
+//Global graphs
 typedef int** matrix_t;
-
-
 matrix_t graph1,graph2,graph3,graph4,graph5,graph6,graph7,graph8,graph9,graph10;
 
-// clock_gettime (CLOCK_MONOTONIC)
-
-// link with realtime library -lrt
+//Must be run with commands; 
+// g++ -lrt -std=c++11 Main.cpp -0 Main
+// ./Main
 
 int main(){
     
+    //Needed for clock_gettime and CLOCK_MONOTONIC
     uint64_t diff;
-	struct timespec start, end;
+    //Start and ending times used for subtracting/calculating the compilation time
+    struct timespec start, end;
     
-    // You can change the seed
+    //Constant seed fo generated random graphs are the same for each algorithm
     srand(123);
     
+    //Dense graphs
     graph1 = adjacencyMatrixGenerator(10, 90, 100, true);
     graph2 = adjacencyMatrixGenerator(100, 90, 100, true);
     graph3 = adjacencyMatrixGenerator(1000, 90, 100, true);
     graph4 = adjacencyMatrixGenerator(2000, 90, 100, true);
     graph5 = adjacencyMatrixGenerator(3000, 90, 100, true);
 
+    //Sparse graphs
     graph6 = adjacencyMatrixGenerator(10, 10, 100, true);
     graph7 = adjacencyMatrixGenerator(100, 10, 100, true);
     graph8 = adjacencyMatrixGenerator(1000, 10, 100, true);
     graph9 = adjacencyMatrixGenerator(2000, 10, 100, true);
     graph10 = adjacencyMatrixGenerator(3000, 10, 100, true);
 
-    
+    //Dijkstra dense
     clock_gettime(CLOCK_MONOTONIC, &start);
     dijkstra(10, graph1, 0);
     clock_gettime(CLOCK_MONOTONIC, &end);
@@ -71,7 +74,7 @@ int main(){
     diff = 1000000000 * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
     cout<<"Compilation time(dij, 3000, dense): "<<diff<< " nanoseconds" << endl;
     
-    
+    //Floyd-Warshell dense
     clock_gettime(CLOCK_MONOTONIC, &start);
     floydWarshell(10, graph1);
     clock_gettime(CLOCK_MONOTONIC, &end);
@@ -107,7 +110,7 @@ int main(){
     diff = 1000000000 * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
     cout<<"Compilation time(floyd, 3000, dense): "<<diff<< " nanoseconds" << endl;
     
-    
+    //Bellman-Ford dense
     clock_gettime(CLOCK_MONOTONIC, &start);
     BellmanFord(10, graph1, 0);
     clock_gettime(CLOCK_MONOTONIC, &end);
@@ -142,14 +145,11 @@ int main(){
     
     diff = 1000000000 * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
     cout<<"Compilation time(bell, 3000, dense): "<<diff<< " nanoseconds" << endl;
-    
-    
-    
    
    
    
    
-   
+    //Dijkstra sparse
     clock_gettime(CLOCK_MONOTONIC, &start);
     dijkstra(10, graph6, 0);
     clock_gettime(CLOCK_MONOTONIC, &end);
@@ -185,7 +185,7 @@ int main(){
     diff = 1000000000 * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
     cout<<"Compilation time(dij, 3000, sparse): "<<diff<< " nanoseconds" << endl;
     
-   
+    //Floyd-Warshell sparse
     clock_gettime(CLOCK_MONOTONIC, &start);
     floydWarshell(10, graph6);
     clock_gettime(CLOCK_MONOTONIC, &end);
@@ -221,7 +221,7 @@ int main(){
     diff = 1000000000 * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
     cout<<"Compilation time(floyd, 3000, sparse): "<<diff<< " nanoseconds" << endl;
     
-    
+    //Bellman-Ford sparse tests
     clock_gettime(CLOCK_MONOTONIC, &start);
     BellmanFord(10, graph6, 0);
     clock_gettime(CLOCK_MONOTONIC, &end);
--- RandGraph.cpp
+++ RandGraph.cpp
@@ -7,11 +7,12 @@ using namespace std;
 
 #define INF2 99999
 
-//Returns an int**, can be treated just like a 2D array of size (n x n).
+//  Returns an int**, can be treated just like a 2D array of size (n x n).
 // "n" is the size of the adjacency Matrix (number of nodes)
 // "percent" is a double for the odds that a given node x will be attached to different node y
 // "largestWeight" is, like is sounds, the largest/heaviest weight allowed.
 // "twoWay" is true if two way paths, false if one way paths
+
 int** adjacencyMatrixGenerator(int n, double percent, int largestWeight, bool twoWay){
     
     //Not necessary
@@ -19,26 +20,33 @@ int** adjacencyMatrixGenerator(int n, double percent, int largestWeight, bool tw
         twoWay = true;
     }
     
+    //Needed for % operations
     largestWeight--;
     
-    
+    //newPercent is calculated by making a proportionate percentage given a graph size    
     double p = (percent * .01) * n; 
-	double newPercent = n + p;
-	newPercent = newPercent - n*2;
+    double newPercent = n + p;
+    newPercent = newPercent - n*2;
     
+    //Initializes the 2D array
     int** adjMatrix = new int*[n];
     for(int a = 0; a < n; a++){
         adjMatrix[a] = new int[n];
     }
+    //Default value
     int m = -1;
+    //If graph is undirected
     if(twoWay){
         for(int i = 0; i < n; i++){
             for(int j = i; j < n; j++){
+		//Generates a number that has the odds of being positive based on newPercent
                 m = newPercent + rand()%n;
+                //If negative, edge does not exist
                 if(m<0){
                     adjMatrix[i][j] = INF2;
                     adjMatrix[j][i] = INF2;
                 }
+                //If positive, edge exists
                 else{
                     m = m%largestWeight + 1;
                     adjMatrix[i][j] = m;
@@ -47,6 +55,7 @@ int** adjacencyMatrixGenerator(int n, double percent, int largestWeight, bool tw
             }
         }
     }
+    //For directed graphs
     else{
         for(int q = 0; q < n; q++){
             for(int w = 0; w < n; w++){
@@ -61,6 +70,7 @@ int** adjacencyMatrixGenerator(int n, double percent, int largestWeight, bool tw
             }
         }
     }
+    //Sets all nodes to have no edges to themselves
     for(int b = 0; b < n; b++){
         adjMatrix[b][b] = 0;
     }
